f9693547bdf4fb53ec947ee9767ff892ee4b8bef
darkeny <darkenpaulo00@gmail.com>
2023-02-04 01:17:30 +0200

    implementes usage GetCountryTime

--- core/src/modules/zones/usage.ts
+++ core/src/modules/zones/usage.ts
@@ -1,11 +1,16 @@
-export class GetCountryTime{
+import { Timezones } from "./factory";
+
+export class GetCountryTime {
+    async run(code: string): Promise<Date> {
+        const offset = Timezones.offset(code)
+        return new Date(Date.now() + offset * 1000 * 60 * 60)
+    }
+}
+
+export class GetCountryTimezone {
 
 }
 
-export class GetCountryTimezone{
+export class GetCountryOffset {
 
-}
-
-export class GetCountryOffset{
-    
 }
